00ac7e90855dcf704838ab347e3a8af12ec6a19b
raxoe <thuraxoe91@gmail.com>
2023-08-02 22:00:12 +0800

    Add Courses  Component and bootstrap js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import './App.css';
 import PageNotFound from './components/PageNotFound';
 import Header from './components/common/Header';
 import { Routes, Route } from 'react-router-dom';
- import HomePage from './components/home/HomePage';
+import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/courses/CoursesPage';
 
 function App() {
   return (
     <div className='container-fluid'>
       <Header />
 
-      {/* <Routes>
-        <Route exact path="/" component={HomePage}></Route>
-        |
-        <Route path="/aboutpage" component={AboutPage}></Route>
-        |
-        <Route component={PageNotFound}/>
-      </Routes> */}
-
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/CoursesPage" element={<CoursesPage />}></Route>
         <Route path="/AboutPage" element={<AboutPage />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,2 @@
+const CoursesPage = () => { return (<h2>Coruses are here</h2>) };
+export default CoursesPage;
